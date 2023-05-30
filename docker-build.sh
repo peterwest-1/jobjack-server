32652b9f193216528c0f7869806744d714e2a189
Pete West <10847430+peterwest-1@users.noreply.github.com>
2023-05-30 17:39:20 +0200

    Updated build script

--- docker-build.sh
+++ docker-build.sh
@@ -1,1 +1,20 @@
-docker build . -t peterwest86/jobjack-server:rest
+#!/bin/bash
+
+# Check if the Git working directory is clean
+if [[ -z $(git status --porcelain) ]]; then
+ 
+    version=$(npm version patch)
+
+    # Extract the version number from the output
+    newVersion=$(echo "$version" | cut -d " " -f 3)
+
+    # Print the new version number
+    echo "New version: $newVersion"
+
+    # You can use the $newVersion variable in your script as needed
+    docker build . -t peterwest86/jobjack-server:$newVersion
+else
+   echo "Working directory not clean"
+fi
+
+exit 0
