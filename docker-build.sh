6c857ca58c6ac2d9e39f3e3fc17deb510d2ad848
Pete West <peterwest86@gmail.com>
2023-05-30 22:07:25 +0200

    Updated build script

--- docker-build.sh
+++ docker-build.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 
-# Check if the Git working directory is clean
 if [[ -z $(git status --porcelain) ]]; then
  
     version=$(npm version patch)
 
-    # Extract the version number from the output
-    newVersion=$(echo "$version" | cut -d " " -f 3)
+    updated=$(echo "$version" | cut -d " " -f 3)
 
-    # Print the new version number
-    echo "New version: $newVersion"
+    echo "New version: $updated"
 
-    # You can use the $newVersion variable in your script as needed
-    docker build . -t peterwest86/jobjack-server:$newVersion
-    docker tag peterwest86/jobjack-server:$newVersion peterwest86/jobjack-server:latest
+    docker build . -t peterwest86/jobjack-server:$updated
+    docker tag peterwest86/jobjack-server:$updated peterwest86/jobjack-server:latest
 else
    echo "Working directory not clean"
 fi
